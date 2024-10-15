20f95177690cc286debecd6230f9b17f96b8e158
Rahul Pulluri <thepulluri@rahuls-mbp.mynetworksettings.com>
2024-10-15 19:11:59 -0400

    Updated index.js  and uploaded picture in assets

--- src/constants/index.js
+++ src/constants/index.js
@@ -3,6 +3,7 @@ import ecommerce from "../assets/projects/ecommerce.png";
 import plant from "../assets/projects/plant.jpeg";
 import bigdata from "../assets/projects/big data.jpeg";
 import movie from "../assets/projects/movies.png";
+import MLBlog from "../assets/projects/MLBlog.png"
 
 export const HERO_CONTENT = `I am a technology professional with experience as an Associate Software Engineer at Accenture and a Software Developer Intern at iThink Logistics. My expertise includes Full Stack Development, Data Engineering, and Cloud Computing. I am currently pursuing a Master of Engineering in Computer Science from Virginia Tech.`;
 
@@ -29,28 +30,25 @@ export const EDUCATIONS = [
 
 export const EXPERIENCES = [
   {
-    year: "June 2024 - Aug 2024",
-    role: "Summer Intern",
+    year: "June 2024 - Present",
+    role: "Research Assistant",
     company: "Virginia Tech",
-    description: `Developed a full-stack event coordination platform that centralized all Virginia Tech events, employing Golang for the back-end CRUD API and React for the front-end. Enhanced back-end performance by implementing structs and slices for lightweight data management, which significantly reduced system overhead and improved response times. Improved API reliability through effective error logging, content type handling, and validation, ensuring seamless interaction between the front-end and back-end components of the application. Additionally, optimized a data pipeline with Python, AWS Lambda, and SQL, boosting data handling efficiency and accelerating data access by 20% across various datasets.`,
-    technologies: ["React.js", "Go", "Python", "CRUD API", "AWS Lambda", "SQL", "PostgreSQL"],
+    description: `Developing a geospatial information visualization platform with Django and React, integrating INRX traffic data through GeoServer and OpenLayers for enhanced location display. Optimizing the data pipeline with Python, AWS Lambda, and SQL, improving data handling efficiency and increasing data access speed by 20% across various datasets.`,
+    technologies: ["Django", "React", "Python", "AWS Lambda", "SQL", "GeoServer", "OpenLayers"],
   },
   {
-    year: "Sept 2022 - May 2023",
+    year: "Sept 2022 - July 2023",
     role: "Associate Software Engineer",
     company: "Accenture",
-    description: `Automated user role management using Node.js and a microservices architecture, enhancing reliability and scalability with Docker and Kubernetes. This implementation improved efficiency, increased role assignment accuracy by 30%, and reduced manual intervention. Developed a scalable pricing module utilizing Java Spring Boot, RESTful APIs, and React, efficiently handling transactional data with MySQL for over 200 product categories. Built a real-time notification system using WebSockets and Redis, delivering notifications to approximately 5,000 users daily and reducing delivery time from 2 minutes to under 10 seconds, significantly enhancing user engagement. Implemented AWS Lambda functions to automate routine data processing tasks, streamlining workflows and reducing manual intervention.`,
-    technologies: ["Java", "Spring Boot", "Node.js", "Django", "React.js", "Node.js", "MySQL", "WebSockets", "Redis", "AWS"],
+    description: `Automated user role management with Java (Spring Boot) in a microservices architecture, improving role assignment accuracy by 30% and reducing manual work. Developed and fine-tuned models for a real-time video processing asset using TensorFlow, incorporating object detection, motion detection, custom training, and JWT-based user authentication. Optimized database schemas for user-role mappings with MySQL and Hibernate, enhancing data retrieval speed. Managed microservices deployment using Docker and Kubernetes, automating scaling and load balancing, while implementing Prometheus and Grafana for real-time monitoring, ensuring 99.9% uptime. Strengthened API security through Role-Based Access Control (RBAC) and OAuth 2.0 authentication, ensuring secure access to sensitive data.`,
+    technologies: ["Java", "Spring Boot", "TensorFlow", "JWT", "Docker", "Kubernetes", "Prometheus, Grafana", "RBAC", "OAuth 2.0"],
   },
   {
     year: "Feb 2022 - Aug 2022",
-    role: "Software Developer Intern",
+    role: "Software Developer",
     company: "iThink Logistics",
-    description: `Developed a React Native app with user registration, OTP verification, real-time tracking, and push notifications to
-enhance user engagement and streamline logistics operations. Collaborated with the DevOps team to implement CI/CD pipelines, improving deployment reliability in a cloud-based environment. Integrated a real-time tracking system using Apache Kafka, MongoDB, and Elasticsearch to optimize the tracking and logistics process, leading to enhanced operational efficiency.
-
-`,
-    technologies: ["React Native", "Node.js", "MongoDB", "Jenkins", "Apache Kafka", "Elasticsearch"],
+    description: `Developed a React Native app with user authentication features and collaborated with the DevOps team to implement CI/CD pipelines, enhancing deployment reliability in a cloud environment. Built a real-time notification system using WebSockets and Redis, delivering notifications to over 5,000 users daily and reducing delivery time from 2 minutes to under 10 seconds.`,
+    technologies: ["React", "Node", "MongoDB", "React Native", "WebSockets", "Redis", "Jenkins", "Cloud"],
   },
 
 ];
@@ -73,15 +71,7 @@ export const PROJECTS = [
     link: "https://github.com/rahulpulluri/E-Commerce-Website",
   },
   {
-    title: "Movie Streaming Platform",
-    image: movie,
-    description:
-      "Built a Netflix-like streaming platform with features including secure user authentication via JWT, movie and TV show browsing, search functionality, and watch history tracking. The platform supports protected routes with middleware, has a responsive design for desktop and mobile, and includes custom error handling. The project uses React.js and Tailwind CSS on the frontend, Node.js and Express.js on the backend, MongoDB for the database, and JWT for authentication.",
-    technologies: ["React.js", "Tailwind CSS", "Node.js", "Express.js", "MongoDB", "JWT"],
-    link: "https://github.com/rahulpulluri/Movie-Streaming-Platform",
-  },
-  {
-    title: "Plant Disease Classification",
+    title: "AgriShield",
     image: plant,
     description:
       "Developed a web application to assist farmers in detecting two common diseases in potato and tomato plants: early blight and late blight. By uploading pictures of plant leaves, farmers can use the app to identify the disease. The model analyzes the images and accurately detects whether the plant is affected by early blight, caused by fungi, or late blight, caused by specific microorganisms, allowing for timely and appropriate treatment to prevent economic losses.",
@@ -95,6 +85,22 @@ export const PROJECTS = [
       "Analyzed social media hashtags, identifying the top 20 hashtags from Twitter data using Apache Spark on Amazon EMR. Configured a transient cluster for cost efficiency and managed data storage with AWS S3.",
     technologies: ["Python", "Apache Spark", "Amazon EMR", "Amazon S3"],
     link: "https://github.com/rahulpulluri/X-Hashtag-Counter-using-Amazon-EMR",
+  },
+  {
+    title: "Machine Learning Blog",
+    image: MLBlog,
+    description:
+      "Created a blog on machine learning that explains key concepts such as probability, classification, clustering, linear and nonlinear regression, and outlier detection. It also includes code samples and data visualizations to clearly understand the concepts.",
+    technologies: ["Python", "Machine learning topics", "Data Visualizations",],
+    link: "https://rahulpulluri.github.io/MLBlog/",
+  },
+  {
+    title: "Movie Streaming Platform",
+    image: movie,
+    description:
+      "Built a Netflix-like streaming platform with features including secure user authentication via JWT, movie and TV show browsing, search functionality, and watch history tracking. The platform supports protected routes with middleware, has a responsive design for desktop and mobile, and includes custom error handling. The project uses React.js and Tailwind CSS on the frontend, Node.js and Express.js on the backend, MongoDB for the database, and JWT for authentication.",
+    technologies: ["React.js", "Tailwind CSS", "Node.js", "Express.js", "MongoDB", "JWT"],
+    link: "https://github.com/rahulpulluri/Movie-Streaming-Platform",
   },
 ];
 
